df9a5155e4a97c13b6d4b8b494f425fc9794c730
Franmicar <franmicarrasquilla@gmail.com>
2026-05-20 17:49:58 +0200

    refactor: use packageController singleton in words endpoint and fix script compilation

--- api/packages/[id]/words.ts
+++ api/packages/[id]/words.ts
@@ -1,19 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { PackageController } from '../../../src/controllers/PackageController';
-import { PackageService } from '../../../src/services/packageService';
-import { PackageRepository } from '../../../src/repositories/PackageRepository';
 import { handleCors } from '../../../src/utils/cors';
+import { authenticateApiKey } from '../../../src/middleware/auth';
+import { container } from '../../../src/config/container';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (handleCors(req, res)) return;
+
+    if (!authenticateApiKey(req, res)) return;
 
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
-    const repository = new PackageRepository();
-    const service = new PackageService(repository);
-    const controller = new PackageController(service);
-
-    await controller.getWords(req, res);
+    await container.packageController.getWords(req, res);
 }
